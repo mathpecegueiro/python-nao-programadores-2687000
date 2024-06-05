2ce8ffd11b701bfca6c5becdd36d52652ec234fb
mathpecegueiro <matheusfpecegueiro@gmail.com>
2024-06-05 15:32:14 +0000

    Exercicio variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Matheus"
 # Declare variáveis de cada tipo numérico
-
-
+troco = 2.20
+temperatura= 15.6
+número_casa=155
 # Declare uma variável do tipo booleano
+verdadeiro= True
